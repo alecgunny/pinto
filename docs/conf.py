b50e1d8b05bdc012e9b5076b08e3b9cd1be397be
Alec Gunny <alec.gunny@gmail.com>
2022-03-22 12:59:41 -0700

    extensions typo

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = "0.0.1"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx.ext.githubpage"]
+extensions = ["myst_parser", "sphinx.ext.githubpages"]
 myst_enable_extensions = [
     "amsmath",
     "colon_fence",
